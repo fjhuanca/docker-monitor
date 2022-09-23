42f6ad4cafc322c016eb564aef4eee16755b0de1
Fernando Huanca <fjhuanca@uc.cl>
2022-09-23 11:43:09 -0300

    minor changes

--- logger.py
+++ logger.py
@@ -21,6 +21,14 @@ if __name__ == "__main__":
                         help="If set, plot will be saved.",
                         default=False, action='store_true')
 
+    parser.add_argument("-rh", "--remote-host", required=False,
+                        help="Set host to monitor, otherwise localhost will be monitored.",
+                        default=None, type=str)
+
+    parser.add_argument("-fn", "--filename", required=False,
+                        help="If set, change filename prefix.",
+                        default=None, type=str)
+
     args = vars(parser.parse_args())
 
 
@@ -29,40 +37,49 @@ if __name__ == "__main__":
     container_id = args["container_id"]
     max_time = args["time"]
     plot_bool = args["plot"]
-
-    client = docker.from_env()
+    client_address = args["remote_host"]
+    filename = args["filename"]
+    if not client_address:
+        client = docker.from_env()
+    else:
+        client = docker.DockerClient(base_url=f"tcp://{client_address}")
     container = client.containers.get(container_id)
     status = container.stats(decode=True, stream = True)
 
     prev_cpu = None
     start_datetime = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H_%M_%S")
     start_time = time.time()
-    filename = f"log_{container_id}_{start_datetime}.csv"
+    if filename:
+        filename = f"log_{start_datetime}_{filename}.csv"
+    else:
+        filename = f"log_{start_datetime}_{container_id}.csv"
     os.makedirs(folder, exist_ok=True)
     os.makedirs(plots_folder, exist_ok=True)
-
-    with open(os.path.join(folder, filename), "w") as file:
-        writter = csv.DictWriter(file, ["time", "datetime", "cpu_percentage", "mem_used_mb"])
-        writter.writeheader()
-        for measure in status:
-            time_now = time.time()
-            datetime_now = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H_%M_%S")
-            current_cpu = measure["cpu_stats"]
-            current_memory = (
-                measure["memory_stats"]["usage"] -
-                measure["memory_stats"]["stats"]["total_inactive_file"]
-                ) / (1024 ** 2)
-            cpu_percentage = get_CPU_percent(prev_cpu, current_cpu)
-            prev_cpu = current_cpu
-            info = {
-                "time": int(time_now - start_time),
-                "datetime": datetime_now,
-                "cpu_percentage": round(cpu_percentage, 2),
-                "mem_used_mb": round(current_memory, 2)
-                }
-            writter.writerow(info)
-            if (time_now - start_time) >= max_time:
-                break
-
+    
+    try:
+        with open(os.path.join(folder, filename), "w") as file:
+            writter = csv.DictWriter(file, ["time", "datetime", "cpu_percentage", "mem_used_mb"])
+            writter.writeheader()
+            for measure in status:
+                time_now = time.time()
+                datetime_now = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H_%M_%S")
+                current_cpu = measure["cpu_stats"]
+                current_memory = (
+                    measure["memory_stats"]["usage"] -
+                    measure["memory_stats"]["stats"]["total_inactive_file"]
+                    ) / (1024 ** 2)
+                cpu_percentage = get_CPU_percent(prev_cpu, current_cpu)
+                prev_cpu = current_cpu
+                info = {
+                    "time": int(time_now - start_time),
+                    "datetime": datetime_now,
+                    "cpu_percentage": round(cpu_percentage, 2),
+                    "mem_used_mb": round(current_memory, 2)
+                    }
+                writter.writerow(info)
+                if max_time > 0 and (time_now - start_time) >= max_time:
+                    break
+    except KeyboardInterrupt:
+        pass
     if plot_bool:
         Plotter(plots_folder, os.path.join(folder, filename))
